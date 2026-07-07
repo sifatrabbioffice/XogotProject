8909cc12e7d4fbf3cbdb71ede4c931c6f0c25f2f
sifatrabbioffice <sifatrabbioffice@gmail.com>
2026-07-07 17:47:53 +0600

    Create godot_iphone.h

---
+++ GodotCore/godot_iphone.h
@@ -0,0 +1,15 @@
+#ifndef godot_iphone_h
+#define godot_iphone_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void godot_iphone_init(void* metal_view);
+void godot_iphone_render(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* godot_iphone_h */
