7afa2f8745847df55d9a94fa2be4170998335bd3
sifatrabbioffice <sifatrabbioffice@gmail.com>
2026-07-07 17:48:48 +0600

    Create PluginMist.h

---
+++ GodotCore/PluginMist.h
@@ -0,0 +1,14 @@
+#ifndef PluginMist_h
+#define PluginMist_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Mist_InitializePlugin();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PluginMist_h */
