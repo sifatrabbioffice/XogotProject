f47a5d9fed480260d157b0006ac6e02ba15e5cb7
sifatrabbioffice <sifatrabbioffice@gmail.com>
2026-07-07 17:45:53 +0600

    Create GodotEngineView.h

---
+++ Bridge/GodotEngineView.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+// SwiftUI তে এই ক্লাসটি ব্যবহারের জন্য objc যোগ করা
+@interface GodotEngineView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame;
+
+@end
+
+NS_ASSUME_NONNULL_END
